1aad22033a9c02396386add70e82454173ead7ed
Dhananjay Ghevde <dhananjay.ghevde@gmail.com>
2025-10-01 11:42:08 -0400

    adding cookie making file

---
+++ make_cookie.py
@@ -0,0 +1,12 @@
+import json
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+from atlas_sdk.client.api import AtlasUIApiClient
+
+if __name__ == "__main__":
+    atlas = AtlasUIApiClient(cookiejar_path="/Users/dhananjay.ghevde/mdbcookie.pickle")
+    #atlas.save_cookiejar("mdbcookie.pickle")
+    result = atlas.get(f"https://cloud.mongodb.com/orgs/64ca747b952bcb462e491b03/groups").json()
+    print("Done retrieving projects: ")
+
+    print("Cookie jar saved to /Users/dhananjay.ghevde/mdbcookie.pickle")
